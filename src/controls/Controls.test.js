89067b37df2bb383fe9bff59a54ecb212e85fc23
doanmade <43007082+doanmade@users.noreply.github.com>
2019-08-19 15:57:31 -0700

    MVP Booyah baby

--- src/controls/Controls.test.js
+++ src/controls/Controls.test.js
@@ -1,4 +1,5 @@
 // Test away
+import { fireEvent, render } from "@testing-library/react";
 import React from "react";
 import renderer from "react-test-renderer"; // 1: install this npm module as a dev dependency
 import Controls from "./Controls";
@@ -10,5 +11,36 @@ describe("<Controls />", () => {
 
     // snapshots are a JSON representation of the DOM tree
     expect(tree.toJSON()).toMatchSnapshot();
+  });
+  it("should toggle locked/unlocked on button fire", () => {
+    const mockFunctionLocked = jest.fn();
+    const { getByText } = render(
+      <Controls locked={true} closed={true} toggleLocked={mockFunctionLocked} />
+    );
+    fireEvent.click(getByText("Unlock Gate"));
+    expect(mockFunctionLocked).toHaveBeenCalled();
+  });
+
+  it("should toggle opened/closed on button fire", () => {
+    const mockFunctionClosed = jest.fn();
+    const { getByText } = render(
+      <Controls
+        locked={false}
+        closed={false}
+        toggleClosed={mockFunctionClosed}
+      />
+    );
+    fireEvent.click(getByText("Close Gate"));
+    expect(mockFunctionClosed).toHaveBeenCalled();
+  });
+
+  it("Should be disabled", () => {
+    const { getByText } = render(<Controls />);
+    expect(getByText("Lock Gate").disabled).toBeTruthy();
+  });
+
+  it("Should be disabled", () => {
+    const { getByText } = render(<Controls locked={true} closed={true} />);
+    expect(getByText("Open Gate").disabled).toBeTruthy();
   });
 });
